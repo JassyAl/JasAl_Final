f120ae30885ce35833f7671fb0f1d12e89c6ee27
JassyAl <jasswalbert@gmail.com>
2023-04-26 18:09:02 -0500

    added cors

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'https://guiltless-synonymous-lock.glitch.me',
+    'http://127.0.0.1:5500',
+    'http://localhost:3500',
+    'http://localhost:3000'
+];
+
+module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const connectDB = require('./config/dbConn');
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 const PORT = process.env.PORT || 3500;
 
 // connect to mongoDB
@@ -12,6 +13,9 @@ connectDB();
 
 // use cors
 app.use(cors());
+
+// use corsOptions
+app.use(cors(corsOptions));
 
 // use express 
 app.use(express.json());
@@ -21,18 +25,18 @@ app.use(express.urlencoded({extended: false}));
 
 // set index to main css page
 app.use("/", express.static(path.join(__dirname, "/css")));
-app.use("/", require("./routes/main"));
+app.use("/", require("./routes/root"));
 app.use("/states", require("./routes/api/routes"));
 
 // catch all
-app.use((req, res, next) => {
+app.all('*', (req, res) => {
   res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({ error: "404 Not Found" });
+  if (req.accepts('html')) {
+      res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+      res.json({ "error": "404 Not Found" });
   } else {
-    res.type("txt").send("404 Not Found");
+      res.type('txt').send("404 Not Found");
   }
 });
 
