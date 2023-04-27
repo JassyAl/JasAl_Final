5fac93673f58e97847b8575a101169e1f7fe5f64
JassyAl <jasswalbert@gmail.com>
2023-04-26 21:34:07 -0500

    added cors allowance

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'https://guiltless-synonymous-lock.glitch.me',
     'https://dazzling-snickerdoodle-777101.netlify.app',
+    'https://jasalfinal.jasminealbert.repl.co',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000'
